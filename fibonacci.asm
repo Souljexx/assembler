5dbe7804fd6ee280fcbe8633abd3ab78837ac91e
Souljexx <84452403+Souljexx@users.noreply.github.com>
2025-01-28 19:12:40 +0100

    Update fibonacci.asm

--- fibonacci.asm
+++ fibonacci.asm
@@ -8,7 +8,7 @@ LOOP:
     ADD A, R0        ; Ajoute \( F(n-2) \) (R0) à A
     ADD A, R1        ; Ajoute \( F(n-1) \) (R1) à A
     MOV R2, A        ; Stocke \( F(n) = F(n-1) + F(n-2) \) dans R22
-	  MOV A, R1        ; Charge \( F(n-1) \) (R1) dans A
+    MOV A, R1        ; Charge \( F(n-1) \) (R1) dans A
     MOV R0, A        ; Met \( F(n-1) \) dans \( F(n-2) \) (R0)
     MOV A, R2        ; Charge \( F(n) \) (R2) dans A
     MOV R1, A        ; Met \( F(n) \) dans \( F(n-1) \) (R1)
